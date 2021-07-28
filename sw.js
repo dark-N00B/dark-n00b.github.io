3daaf79aa0bf89ad0aade4528724aff52e3b49a5
darkNOOB <darkn00b@protonmail.com>
2021-07-28 05:43:47 +0530

    Dmmy-added js files in 'sw.js'

--- sw.js
+++ sw.js
@@ -5,6 +5,19 @@ self.addEventListener('install', function(event) {
               // Static files that make up the application shell are cached
             return cache.add('index.html');  // If you have css file and app.js files
               // Please add here as well, to be cached!
+            
+              // assets/js/jquery-3.5.1.min.js
+              // assets/js/bootstrap.bundle.min.js
+              // assets/vendor/owl-carousel/owl.carousel.min.js
+              // assets/vendor/perfect-scrollbar/js/perfect-scrollbar.js
+              // assets/vendor/isotope/isotope.pkgd.min.js
+              // assets/vendor/nice-select/js/jquery.nice-select.min.js
+              // assets/vendor/fancybox/js/jquery.fancybox.min.js
+              // assets/vendor/wow/wow.min.js
+              // assets/vendor/animateNumber/jquery.animateNumber.min.js
+              // assets/vendor/waypoints/jquery.waypoints.min.js
+              // assets/js/google-maps.js
+              // assets/js/topbar-virtual.js
         })
     );
 });
